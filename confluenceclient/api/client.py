98e9e53acb4e8cbbdf2409d8f5e4405dbeb7b946
Xavier Lucas <xavier.lucas.2@gmail.com>
2017-03-26 23:36:23 +0200

    RPC client
    Signed-off-by: Xavier Lucas <xavier.lucas.2@gmail.com>

---
+++ confluenceclient/api/client.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2017  Xavier Lucas
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import xmlrpclib
+
+from confluenceclient.api import exceptions as ex
+
+
+class Client:
+    def __init__(self, host, use_tls=False):
+        scheme = 'https' if use_tls else 'http'
+        try:
+            url = "%s://%s/rpc/xmlrpc" % (scheme, host)
+            self.soap_client = xmlrpclib.Server(url)
+        except xmlrpclib.Fault, e:
+            raise ex.RPCException(e)
---
+++ confluenceclient/api/exceptions.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2017  Xavier Lucas
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+class RPCException(Exception):
+    def __init__(self, rpc_fault):
+        self.message = "%d: %s" % (rpc_fault.faultCode, rpc_fault.faultString)
+        super(RPCException, self).__init__(self.message)
